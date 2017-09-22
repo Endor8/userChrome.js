f3681c79ef8cccb70f38bca6bbe54127248ad6b0
Endor8 <endor8@gmail.com>
2017-09-22 06:46:33 +0200

    Neue Version von aborix

--- Firefox-57/MovableMenuButton.uc.js
+++ Firefox-57/MovableMenuButton.uc.js
@@ -1,40 +1,37 @@
 (function() {
-   if (location != 'chrome://browser/content/browser.xul')
-      return;
-
-   try {
+  if (location != 'chrome://browser/content/browser.xul')
+    return;  
+  var menuButton = document.getElementById('PanelUI-menu-button');
+ 
+  if (!CustomizableUI.getPlacementOfWidget('ucjs_PanelUI-button')) { 
+    try {
       CustomizableUI.createWidget({
-         id: 'ucjs_PanelUI-menu-button',
-         type: 'custom',
-         defaultArea: CustomizableUI.AREA_NAVBAR,
-         onBuild: function(aDocument) {
-            var button = aDocument.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'toolbarbutton');
-            var attributes = {
-               id: 'ucjs_PanelUI-menu-button',
-               class: 'toolbarbutton-1 chromeclass-toolbar-additional',
-               type: 'menu',
-               removable: 'true',
-               label: 'Firefox Anpassen',
-               tooltiptext: 'Firefox Anpassen + Panel',
-               onclick: 'if (event.button == 0) event.target.ownerGlobal.PanelUI.toggle(event)',
-               style: 'list-style-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAJElEQVQ4jWNgGEzgAgMDw38i8XlKDbhAO38MKBgNRCqAkRqIANHrRYKU+i0FAAAAAElFTkSuQmCC)'
-            };
-            for (var a in attributes) {
-               button.setAttribute(a, attributes[a]);
-            };
-            return button;
-         }
+        id: 'ucjs_PanelUI-button',
+        type: 'custom',
+        defaultArea: CustomizableUI.AREA_NAVBAR,
+        onBuild: function(aDocument) {
+          var toolbaritem = aDocument.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'toolbaritem');
+          toolbaritem.id = 'ucjs_PanelUI-button';
+          toolbaritem.className = 'chromeclass-toolbar-additional';          
+          toolbaritem.setAttribute('label', 'Firefox Anpassen');          
+          return toolbaritem;            
+        }
       });
+      if (!CustomizableUI.getPlacementOfWidget('ucjs_PanelUI-button')) {     
+        menuButton.style.display = 'none';
+        return; 
+      };        
+    } catch(e) {        
+      menuButton.style.display = 'none';
+      return;      
+    };    
+  };
 
-      var css = '\
-         @-moz-document url("chrome://browser/content/browser.xul") { \
-            #PanelUI-button {display: none} \
-            #ucjs_PanelUI-menu-button > dropmarker {display: none}\
-         }';
-      var uri = makeURI('data:text/css,' + encodeURIComponent(css));
-      var SSS = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-      SSS.loadAndRegisterSheet(uri, SSS.AUTHOR_SHEET);
-
-   } catch(e) { };
+  setTimeout(function() { 
+    document.getElementById('ucjs_PanelUI-button').appendChild(menuButton);       
+    menuButton.setAttribute('consumeanchor', 'ucjs_PanelUI-button');
+    menuButton.setAttribute('label', 'Firefox Anpassen');
+    menuButton.setAttribute('tooltiptext', 'Firefox Anpassen + Panel');        
+  }, 0);
 
 }());
