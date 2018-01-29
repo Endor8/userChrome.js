fb44596b4e522725aa799857f740c0479c70efd9
Endor8 <endor8@gmail.com>
2018-01-29 20:42:31 +0100

    Add files via upload

--- webdevelopertoolbarbutton/WebDeveloper-Menu.uc.js
+++ webdevelopertoolbarbutton/WebDeveloper-Menu.uc.js
@@ -1,21 +1,32 @@
-    //      Webdevelopercontextmenu.uc.js
+//   WebDeveloper-Menu.uc.js
 
-    (function() {
+(function() {
+   if (location != 'chrome://browser/content/browser.xul')
+      return;
+   setTimeout(function() {
+      if (document.getElementById('menuWebDeveloperPopup').childElementCount <= 5) {
+         // s. DevToolsStartup.prototype.initDevTools
+         // https://dxr.mozilla.org/mozilla-central/source/devtools/shim/devtools-startup.js
+         let { require } = Cu.import("resource://devtools/shared/Loader.jsm", {});
+         require("devtools/client/framework/devtools-browser");
+      };
+      var dblMenu = document.getElementById('webDeveloperMenu').cloneNode(true);
+      document.getElementById('contentAreaContextMenu').insertBefore(dblMenu, document.getElementById('context-viewsource'));
+      dblMenu.id = 'context-' + dblMenu.id;
+      var elements = dblMenu.getElementsByTagName('*');
+      for (let elem of elements) {
+         let origId = elem.id;
+         if (origId) {
+            elem.id = 'context-' + origId;
+            if (elem.tagName == 'menuitem') {
+               elem.setAttribute('oncommand', 'document.getElementById("' + origId + '").click();');
+               let obs = document.createElement('observes');
+               obs.setAttribute('element', origId);
+               obs.setAttribute('attribute', 'checked');
+               elem.appendChild(obs);
+            };
+         };
+      };
+   }, 100);
+}());
 
-       if (location != 'chrome://browser/content/browser.xul') return;
-          setTimeout( function() {
-
-       var contextMenu = document.getElementById('contentAreaContextMenu');
-       var dblMenu = document.importNode(document.getElementById('webDeveloperMenu'), true);
-    //   contextMenu.appendChild(document.createElement('menuseparator'));
-    //   contextMenu.appendChild(dblMenu);
-       contextMenu.insertBefore(dblMenu, document.getElementById('context-viewsource'));
-       dblMenu.id = 'context-' + dblMenu.id;
-       var elements = dblMenu.getElementsByTagName('*');
-       for (let i=0; i<elements.length; i++) {
-          var elem = elements[i];
-          if (elem.id)
-             elem.id = 'context-' + elem.id;
-       };
-    }, 0);
-    }());
