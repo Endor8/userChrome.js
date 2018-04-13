8db0d20701159bb212bf1c212267027850d569cf
Endor8 <endor8@gmail.com>
2018-04-13 20:43:03 +0200

    Aktualisierung von aborix

--- Firefox-57/memoryMonitormod.uc.js
+++ Firefox-57/memoryMonitormod.uc.js
@@ -1,10 +1,11 @@
-// ==UserScript== 
+// ==UserScript==
 // @name           MemoryMonitorMod.uc.js
 // @description    Einfacher FF-Speicher-Monitor
 // @include        main
 // @charset        UTF-8
 // @note           Basiert auf dem Script MemoryMonitorMod.uc.js Funktioniert auch mit FF57+
 // ==/UserScript==
+
 var ucjsMM = {
   // Update-Intervall[ms]
   _interval: 5000,
@@ -13,46 +14,31 @@ var ucjsMM = {
   // Speicher-Einheit: B, KB, KiB, MB, MiB, GB, GiB
   _prefix: " MB",
   // Speicher-Einheitanzeige in der Statusbar
-  _dPrefix: true,
+  _dPrefix: false,
   // automatischen Restart bei Überschreitung von _maxMemory durchführen
   _autoRestart: false,
 
 	interval : null,
 	init : function () {
-/*
-		var memoryPanel = document.createElement('statusbarpanel');
-		memoryPanel.id = 'MemoryDisplay';
-		memoryPanel.setAttribute('label', ucjsMM._MemoryValue + ucjsMM._prefix);
-		memoryPanel.setAttribute('tooltiptext', 'Speicher Monitor. Klick öffnet about:memory');
-*/
 		var memoryPanel = document.createElement('toolbaritem');
 		memoryPanel.id = 'MemoryDisplay';
+		memoryPanel.setAttribute('tooltiptext', 'Speicher Monitor. Klick öffnet about:memory');
 		var label = document.createElement('label');
 		label.setAttribute('value', ucjsMM._MemoryValue + ucjsMM._prefix);
 		memoryPanel.appendChild(label);
-
-	//  document.getElementById('bottom-toolbar').insertBefore(memoryPanel, document.getElementById('eom-button'));
-	    document.getElementById('nav-bar-customization-target').insertBefore(memoryPanel, document.getElementById('search-container'));
-	//  document.getElementById('addon-bar').insertBefore(memoryPanel, document.getElementById('uAutoPagerize-icon'));
-	//  document.getElementById('urlbar-icons').insertBefore(memoryPanel, document.getElementById('uAutoPagerize-icon'));
+		document.getElementById('nav-bar-customization-target').insertBefore(memoryPanel, document.getElementById('search-container'));
 		this.start();
 		this.interval = setInterval(this.start, this._interval);
 	},
 	start : function () {
 		try {
-			const Cc = Components.classes;
-			const Ci = Components.interfaces;
 			var MemReporters = Cc['@mozilla.org/memory-reporter-manager;1'].getService(Ci.nsIMemoryReporterManager);
 			var workingSet = MemReporters.resident;
 			ucjsMM._MemoryValue = Math.round(workingSet / (1024 * 1024));
-			var restartMemory = ucjsMM._MaxMemory * 1024 * 1024;
+			var restartMemory = ucjsMM._maxMemory * 1024 * 1024;
 			var memoryPanel = document.getElementById('MemoryDisplay');
-
-//			memoryPanel.setAttribute('label', ucjsMM.addFigure(ucjsMM._MemoryValue) + ucjsMM._prefix);
-
-			memoryPanel.firstChild.setAttribute('value', ucjsMM.addFigure(ucjsMM._MemoryValue) + ucjsMM._prefix);   
-      
-			memoryPanel.setAttribute('onclick', "openUILinkIn('about:memory','tab')");
+			memoryPanel.firstChild.setAttribute('value', ucjsMM.addFigure(ucjsMM._MemoryValue) + ucjsMM._prefix);
+			memoryPanel.setAttribute('onclick', "if (event.button == 0) openUILinkIn('about:memory', 'tab', {})");
 			if (workingSet > restartMemory) {
 				if (memoryPanel.style.backgroundColor == 'red' && ucjsMM._autoRestart)
 					ucjsMM.restart();
@@ -73,9 +59,6 @@ var ucjsMM = {
 		while (num != (num = num.replace(/^(-?\d+)(\d{3})/, '$1,$2')));
 		return num;
 	},
-	restart : function () {
-		var appStartup = Components.interfaces.nsIAppStartup;
-		Components.classes['@mozilla.org/toolkit/app-startup;1'].getService(appStartup).quit(appStartup.eRestart | appStartup.eAttemptQuit);
-	},
+	restart : BrowserUtils.restartApplication
 }
 ucjsMM.init();
