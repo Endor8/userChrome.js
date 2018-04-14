cbdc848550c9ec8fc112a32b7a8a9f2633a555b1
Endor8 <endor8@gmail.com>
2018-04-14 19:21:27 +0200

    Aktualisierung auf Version 0.8.2

--- Firefox-57/SearchEngineWheelScroll.uc.js
+++ Firefox-57/SearchEngineWheelScroll.uc.js
@@ -2,39 +2,40 @@
 //Einstellungen	- true = ein(aktiviert) false = aus(deaktiviert)
 
 //Namen und Symbol der Suchmaschine in der Suchleiste anzeigen
-  let label = true; //Namen anzeigen
-  let img = true;	//Symbol - Favicon anzeigen
-//Doppelklick auf Suchleiste, um zur Standard Suchmaschine zurück zukehren
-  let only = false;	//Suchleiste leeren und nicht zur Standardsuchmaschine zurückkehren [Neu in Version 0.6]
-  let dbl = true;	//Funktion aktivieren
-  let zero = false;	//Bei Klick zur obersten Suchmaschine zurückkehren
-  let select = 'Google'; //Standard Suchmaschine angeben, zum Beispiel 'DuckDuckGo'.
-  let erase = true; //Nach Suche Suchleiste leeren
+  const BUTTON_ONLY = false	//Nur bei Suchleisten Suchschaltfläche anwenden - Suchmaschinen wechseln mit Scrollrad im Hauptkontextmenü deaktivieren
+  const label = true; //Namen anzeigen
+  const img = true;	//Symbol - Favicon anzeigen
+//Doppelklick auf Suchleiste, um zur Standard Suchmaschine zurück zu kehren
+  const only = false; //Suchleiste leeren und nicht zur Standardsuchmaschine zurückkehren [Neu in Version 0.6]
+  const dbl = true;	//Funktion aktivieren
+  const zero = false; //Bei Klick zur obersten Suchmaschine zurückkehren
+  const select = 'Google Deutschland'; //Standard Suchmaschine angeben, zum Beispiel 'DuckDuckGo'.
+  const erase = true; //Nach Suche Suchleiste leeren
 //[Aktion nach dem Suchen mit der Suchleiste]
-  let auto = true;	//Andere Einstellungen verwenden, durch einen Doppelklick auf die Suchleiste  
+  const auto = true; //Andere Einstellungen verwenden, durch einen Doppelklick auf die Suchleiste  
 //Kontextmenü Suche wechseln mit[～～Suchen]
-  let cxt = true;	//Funktion aktivieren
-  let icon = true;	//Symbol - Favicon anzeigen
-  let clk = true;	//Klicken, um zur Standard Suchmaschine zurückzukehren (Andere Einstellungen verwenden ~ mit Doppelklick auf die Suchleiste)
-  let sync = false;	//Suchwort an die Suchleiste senden [Neu in Version 0.7]
-  let hist = true;	//Suchwort der Suchchronik der Suchleiste hinzufügen * Es ist OK, wenn die Synchronisation inkorrekt ist [Neu in Version 0.8]
+  const cxt = true;	//Funktion aktivieren
+  const icon = true; //Symbol - Favicon anzeigen
+  const clk = true;	//Klicken, um zur Standard Suchmaschine zurückzukehren (Andere Einstellungen verwenden ~ mit Doppelklick auf die Suchleiste)
+  const sync = false; //Suchwort an die Suchleiste senden [Neu in Version 0.7]
+  const hist = true; //Suchwort der Suchchronik der Suchleiste hinzufügen * Es ist OK, wenn die Synchronisation inkorrekt ist [Neu in Version 0.8]
 //[Verhalten beim Start] * Gilt auch beim Neustart
-  let start0 = false; //Andere Einstellungen verwenden, durch Doppelklick auf die Suchleiste
+  const start0 = false; //Andere Einstellungen verwenden, durch Doppelklick auf die Suchleiste
 
 //Konfiguration
 
   const scrollRight = true;
   let bar = document.getElementById('searchbar');
+  let box = bar.textbox.inputField;
   let menu = document.getElementById('context-searchselect');
   let BSS = Components.classes["@mozilla.org/browser/search-service;1"]
   			.getService(Components.interfaces.nsIBrowserSearchService);
   
   if(!!dbl) bar.addEventListener('dblclick', ResetE, false);
   bar.addEventListener('DOMMouseScroll', ChangeE, false);
-if(!!auto) bar.addEventListener('load', autoreset, {once:true});
   if(!!cxt) menu.addEventListener('wheel', ChangeE, false);
   if(!!clk) menu.addEventListener('click', function(){
-  	if(!!sync) {bar.textbox.inputField.value = this.searchTerms}else{bar.textbox.inputField.value = bar.textbox.inputField.value}
+  	if(!!sync) {box.value = this.searchTerms}else{box.value = box.value}
   	if(!!hist) SyncHistory();
   	setTimeout(function(){ResetE()}, 0)
   }, false);
@@ -44,7 +45,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
         bar.removeEventListener('DOMMouseScroll', ChangeE, false);
         menu.removeEventListener('wheel', function(e){if(!!con) ChangeE(e)} , false);
         menu.removeEventListener('click', function(){
-        	if(!!sync) {bar.textbox.inputField.value = this.searchTerms}else{bar.textbox.inputField.value = bar.textbox.inputField.value}
+        	if(!!sync) {box.value = this.searchTerms}else{box.value = box.value}
         	if(!!hist) SyncHistory();
         	setTimeout(function(){ResetE()}, 0)
         } , false);
@@ -60,7 +61,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
   		BSS.currentEngine = BSS.getEngineByName(select)
 	  	}
   	}
-  	if(!!erase || !!only) bar.textbox.inputField.value = '';
+  	if(!!erase || !!only) box.value = '';
   }
   
   function CMenu() {
@@ -70,7 +71,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
   		let truncChar = selectedText[15].charCodeAt(0);
   		if (truncChar >= 0xDC00 && truncChar <= 0xDFFF)
   			truncLength++;
-  		selectedText = selectedText.substr(0, truncLength) + 'Suchen mit ';
+  		selectedText = selectedText.substr(0, truncLength) + '\u2026';
   	}
   	var menuLabel = gNavigatorBundle.getFormattedString('contextMenuSearch',[BSS.currentEngine.name, selectedText]);
   	menu.setAttribute('label', menuLabel);
@@ -99,6 +100,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
   }
   
   function ChangeE(event) {
+  	if(BUTTON_ONLY && event.originalTarget.className !== 'searchbar-search-button') return;
     let dir = (scrollRight ? 1 : -1) * Math.sign(event.detail || event.deltaY);
   	this.engines = BSS.getVisibleEngines({});
   	let index = this.engines.indexOf(BSS.currentEngine);
@@ -109,7 +111,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
   
   function ShowCurrentE(){
   	let E = BSS.currentEngine;
-		if(!!label && !!bar.textbox)bar.textbox.inputField.setAttribute('placeholder', E.name);
+		if(!!label)box.setAttribute('placeholder', E.name);
 	let icon = document.getAnonymousElementByAttribute(bar, 'class', 'searchbar-search-icon');
 		if(!!img)icon.setAttribute('style', "list-style-image: url('"+ E.iconURI.spec +"') !important; -moz-image-region: auto !important; width: 16px !important; padding: 2px 0 !important;");
   }
@@ -118,12 +120,12 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
     window.addEventListener("unload", () => {
       Services.obs.removeObserver(observe, "browser-search-engine-modified");
   });
-/* 
+   
   Services.obs.addObserver(ob2, "browser-search-service");
     window.addEventListener("unload", () => {
       Services.obs.removeObserver(ob2, "browser-search-service");
   });
- */
+    
    
    function observe(aEngine, aTopic, aVerb) { 
     if (aTopic == "browser-search-engine-modified") {
@@ -133,7 +135,7 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
       	CMenu();
   	}
   }
-/* 
+
    function ob2(aSubject, aTopic, aData) {
     if(aData === "init-complete" && aTopic === "browser-search-service") {
        if(!!start0) ResetE();
@@ -141,15 +143,11 @@ if(!!auto) bar.addEventListener('load', autoreset, {once:true});
    	   CMenu();
    	 }
    }
-*/  
-  function autoreset(){
-  	if(!!start0) ResetE();
-  	ShowCurrentE();
-  	CMenu();
+    
+  if(!auto) return;
   	bar.cmd = bar.doSearch;
   	bar.doSearch = function(aData, aWhere, aEngine) {
   	this.cmd(aData, aWhere, aEngine);
   	ResetE()
-    }  
-  }
+  }  
 })()
