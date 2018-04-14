ce547082452312100a13ab5ea957dabdcea2356f
Endor8 <endor8@gmail.com>
2018-04-14 19:20:26 +0200

    Update ContextTranslate.uc.js

--- Firefox-57/ContextTranslate.uc.js
+++ Firefox-57/ContextTranslate.uc.js
@@ -7,7 +7,7 @@
 			let t = (message.data !== '');
 			let e = (document.charset || document.characterSet);
 			if (t) {
-				gBrowser.loadOneTab('http://translate.google.com/#auto/de/' + message.data, null, null, null, false, false);
+				gBrowser.loadOneTab('http://translate.google.com/#auto/de/' + encodeURIComponent(message.data), null, null, null, false, false);
 			} else {
 				gBrowser.loadOneTab('http://translate.google.com/translate?u=' + gBrowser.currentURI.spec + '&hl=de-DE&ie=' + e + '&sl=auto&tl=de-DE', null, null, null, false, false);
 			};
