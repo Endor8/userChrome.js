37945a77ceef4b79d0b49869a2d912c52cfc52da
Endor8 <endor8@gmail.com>
2018-08-16 08:04:34 +0200

    Add files via upload

---
+++ Neue Scripte 2018/newtabfromhistory.uc.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name           newtabfromhistory.uc.js
+// @namespace      https://www.camp-firefox.de/forum/viewtopic.php?p=1090093#p1090093
+// @description    Links aus Chronik in neuem Tab öffnen
+// @include        main
+// @author         aborix
+// @compatibility  61+
+// @version        2018/08/15 21:16 Erste Version
+// ==/UserScript==
+
+(function() {
+
+  if (location != 'chrome://browser/content/browser.xul')
+    return;
+
+  eval('PlacesUIUtils.openNodeWithEvent = '  + PlacesUIUtils.openNodeWithEvent.toString()
+    .replace('PlacesUtils.nodeIsBookmark(aNode) && ', ''));
+
+  let onPopupshowing = function() {
+    let historyMenu = document.getElementById('history-menu');
+    if (!historyMenu._placesView) {
+      new HistoryMenu(event);
+      historyMenu._placesView._onCommand = function HM__onCommand(aEvent) {
+        let placesNode = aEvent.target._placesNode;
+        if (placesNode) {
+          PlacesUIUtils.openNodeWithEvent(placesNode, aEvent);
+        };
+      };
+    };
+  };
+
+  let historyPopup = document.getElementById('goPopup');
+  historyPopup.setAttribute('onpopupshowing', '(' + onPopupshowing.toString() + ')()');
+
+})();
