41e6228c18707cbea5bb1adb34d3214e97e264f8
Endor8 <endor8@gmail.com>
2018-09-06 07:39:32 +0200

    vbox id eingefügt

--- Neue Scripte 2018/newtoolbar.uc.js
+++ Neue Scripte 2018/newtoolbar.uc.js
@@ -12,6 +12,7 @@ tb.setAttribute('context', 'toolbar-context-menu');
 let parentE = document.getElementById('PersonalToolbar').parentNode;
 let childE = document.getElementById('PersonalToolbar');
 let vbox = document.createElement('vbox');
+vbox.id = 'uc-tb2-vbox';
 vbox.appendChild(tb);
 parentE.insertBefore( vbox , childE );
 vbox.style.backgroundColor = '#E7E7E7'; //als Wert geht: red, green, yellow oder black, aber auch zbs. #f7f7f7 usw.
