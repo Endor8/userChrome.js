dfa6a3ea209980b18718312c66771c30fb2b2905
Endor8 <endor8@gmail.com>
2019-03-14 19:41:30 +0100

    Aktualisierung bei aborix

--- Url-tooltip/Url-tooltip-mod.uc.js
+++ Url-tooltip/Url-tooltip-mod.uc.js
@@ -1,4 +1,4 @@
-﻿// userChromeJS
+// userChromeJS - title, alt, url
 
 (function() {
 
@@ -8,54 +8,74 @@
   var frameScript = function() {
 
     addEventListener('pageshow', function(event) {
+
       var document = event.target;
+      if (document.doingTooltipScript)
+        return;
+      document.doingTooltipScript = true;
+
       var documentElement = document.documentElement;
 
+      var tooltipElement;
+      var makeTooltip = false;
+      var titleElement = null;
+      var title = '';
+
       documentElement.addEventListener('mouseover', function(event) {
+
         var element = event.target;
-        var elementsWithTitle = [];
-        var elementWithAlt = null;
-        while (element != documentElement && !element.href) {
-          if (element.hasAttribute('title')) {
-            elementsWithTitle.push(element);
+        tooltipElement = element;
+
+        var alt = '';
+        var href = '';
+
+        while ((!alt || !href || !titleElement) && element != documentElement) {
+          if (!alt && element.alt) {
+            alt = element.alt;
+            makeTooltip = true;
           };
-          if (element.alt) {
-            elementWithAlt = element;
+          if (!href && element.href && element.getAttribute('href') != '\u0023'
+              && element.protocol != 'javascript:') {
+            href = element.href;
+            makeTooltip = true;
+          };
+          if (!titleElement && element.hasAttribute('title')) {
+            titleElement = element;
           };
           element = element.parentNode;
         };
 
-        if (element.href && !element.checkedTooltip) {
-          element.checkedTooltip = true;
-          if (element.getAttribute('href') != '\u0023' && element.protocol != 'javascript:') {
+        if (makeTooltip) {
 
-            let title = element.title;
-            if (!title) {
-              let length = elementsWithTitle.length;
-              if (length > 0) {
-                title = elementsWithTitle[length - 1].title;
-              };
-            };
-            if (title) {
-              title += '\n';
-            };
-
-            let alt = '';
-            if (element.alt) {
-              alt = element.alt + '\n';
-            } else if (elementWithAlt) {
-              alt = elementWithAlt.alt + '\n';
-            };
-
-            element.title = title + alt + element.href;
-
-            for (let elem of elementsWithTitle) {
-              elem.removeAttribute('title');
-            };
+          if (titleElement) {
+            title = titleElement.title;
+            titleElement.removeAttribute('title');
           };
+
+          let titleSep = title ? '\n' : '';
+          let altSep = alt ? '\n' : '';
+          let altPrefix = (alt && !title) ? 'a: ' : '';
+
+          tooltipElement.title = title + titleSep + altPrefix + alt + altSep + href;
+
         };
 
       });
+
+      documentElement.addEventListener('mouseout', function(event) {
+
+        if (makeTooltip) {
+          tooltipElement.removeAttribute('title');
+          if (titleElement) {
+            titleElement.setAttribute('title', title);
+            title = '';
+          };
+          makeTooltip = false;
+        };
+        titleElement = null;
+
+      });
+
     });
 
   };
