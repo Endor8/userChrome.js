89233c16040bad846e545afaa8cbfb6b8e8c2b1d
Endor8 <endor8@gmail.com>
2019-09-06 19:18:32 +0200

    Aktualisierung vom Autor

--- Mutirowtabs/Firefox-69/1-MultiRowTabLiteforFx.uc.js
+++ Mutirowtabs/Firefox-69/1-MultiRowTabLiteforFx.uc.js
@@ -3,7 +3,7 @@
 // @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
 // @description    Experimentelle CSS Version für Mehrzeilige Tableiste
 // @include        main
-// @compatibility  Firefox 67
+// @compatibility  Firefox 69
 // @author         Alice0775
 // @version        2016/08/05 00:00 Firefox 48
 // @version        2016/05/01 00:01 hide favicon if busy
@@ -14,15 +14,14 @@
 "user strict";
 MultiRowTabLiteforFx();
 function MultiRowTabLiteforFx() {
-    var css =`
+    var css =` @-moz-document url-prefix("chrome://browser/content/browser.xhtml") {
     /* Mehrzeilige Tableiste */
     tabs>arrowscrollbox{display:block;}
-    tabs arrowscrollbox>scrollbox{display:flex;flex-wrap:wrap;}
+    tabs arrowscrollbox>scrollbox{display:flex;display:-webkit-box;flex-wrap:wrap;}
     tabs tab[fadein]:not([pinned]){flex-grow:1;}
     tabs tab,.tab-background {
         height: var(--tab-min-height);
-        z-index: 1 !important;
-    }
+        z-index: 1 !important; }
     tab>.tab-stack{width:100%;}
     [sizemode="fullscreen"] #TabsToolbar>#window-controls,
     .titlebar-buttonbox-container>.titlebar-buttonbox{display:block;}
@@ -31,10 +30,10 @@ function MultiRowTabLiteforFx() {
        Links und rechts → hbox.titlebar-spacer 
        links → hbox.titlebar-spacer[type="pre-tabs"] 
        rechts → hbox.titlebar-spacer[type="post-tabs"] */
-    hbox.titlebar-spacer,
+    hbox.titlebar-spacer
     /* Ausblenden - Verstecken */
-    #alltabs-button,tabs [class^="scrollbutton"],tabs spacer,[autohide="true"][inactive="true"] .titlebar-buttonbox { display: none; }
-    `;
+    ,#alltabs-button,tabs [class^="scrollbutton"],tabs spacer,[autohide="true"][inactive="true"] .titlebar-buttonbox { display: none; }
+    } `;
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
     sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
@@ -49,23 +48,23 @@ function MultiRowTabLiteforFx() {
     'type="text/css" href="data:text/css,' + encodeURIComponent(style) + '"');
     document.insertBefore(sspi, document.documentElement);
     gBrowser.tabContainer._animateTabMove = function(event){}
-    gBrowser.tabContainer._finishAnimateTabMove = function(event){}
+    gBrowser.tabContainer._finishAnimateTabMove = function(){}
     gBrowser.tabContainer.lastVisibleTab = function() {
-        var tabs = this.children;
+        var tabs = this.allTabs;
         for (let i = tabs.length - 1; i >= 0; i--){
             if (!tabs[i].hasAttribute("hidden"))
                 return i;
         }
         return -1;
-    };
+    }
     gBrowser.tabContainer.clearDropIndicator = function() {
-        var tabs = this.children;
+        var tabs = this.allTabs;
         for (let i = 0, len = tabs.length; i < len; i++){
             let tab_s= tabs[i].style;
             tab_s.removeProperty("border-left-color");
             tab_s.removeProperty("border-right-color");
         }
-    };
+    }
     gBrowser.tabContainer.addEventListener("dragleave",gBrowser.tabContainer.clearDropIndicator, false);
     gBrowser.tabContainer._onDragOver = function(event) {
         event.preventDefault();
@@ -74,48 +73,100 @@ function MultiRowTabLiteforFx() {
         var newIndex = this._getDropIndex(event);
         if (newIndex == null)
             return;
-        if (newIndex < this.children.length) {
-            this.children[newIndex].style.setProperty("border-left-color","red","important");
+        let children = this.allTabs;
+        if (newIndex < children.length) {
+            children[newIndex].style.setProperty("border-left-color","red","important");
         } else {
             newIndex = gBrowser.tabContainer.lastVisibleTab();
             if (newIndex >= 0)
-                this.children[newIndex].style.setProperty("border-right-color","red","important");
+                children[newIndex].style.setProperty("border-right-color","red","important");
         }
-    };
-    gBrowser.tabContainer.addEventListener("dragover", gBrowser.tabContainer._onDragOver, false);
-    gBrowser.tabContainer._getDropIndex = function(event, isLink) {
-        var tabs = this.children;
-        var tab = this._getDragTargetTab(event, isLink);
-        if (!RTL_UI) {
-            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++)
-                if (event.screenX < tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
-                 && event.screenY < tabs[i].screenY + tabs[i].getBoundingClientRect().height) // multirow fix
-                    return i;
-        } else {
-            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++)
-                if (event.screenX > tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
-                 && event.screenY > tabs[i].screenY + tabs[i].getBoundingClientRect().height) // multirow fix
-                    return i;
-        }
-        return tabs.length;
     }
+    gBrowser.tabContainer.addEventListener("dragover", gBrowser.tabContainer._onDragOver, false);
     gBrowser.tabContainer.onDrop = function(event) {
         this.clearDropIndicator();
         var dt = event.dataTransfer;
         var draggedTab;
         if (dt.mozTypesAt(0)[0] == TAB_DROP_TYPE) {
             draggedTab = dt.mozGetDataAt(TAB_DROP_TYPE, 0);
-            if (!draggedTab)
+            if (!draggedTab) {
                 return;
+            }
         }
-        this._tabDropIndicator.collapsed = true;
+        this._tabDropIndicator.hidden = true
         event.stopPropagation();
-        if (draggedTab && draggedTab.parentNode == this) {
+        if (draggedTab && draggedTab.container == this) {
             let newIndex = this._getDropIndex(event, false);
             if (newIndex > draggedTab._tPos)
                 newIndex--;
             gBrowser.moveTabTo(draggedTab, newIndex);
         }
-    };
+    }
     gBrowser.tabContainer.addEventListener("drop",gBrowser.tabContainer.onDrop, false);
+    gBrowser.tabContainer._getDragTargetTab = function(event, isLink) {
+        let tab = event.target;
+        while (tab && tab.localName != "tab") {
+            tab = tab.parentNode;
+        }
+        if (tab && isLink) {
+            let { width } = tab.getBoundingClientRect();
+            if (
+                event.screenX < tab.screenX + width * 0.25 ||
+                event.screenX > tab.screenX + width * 0.75
+            ) {
+                return null;
+            }
+        }
+        return tab;
+    }
+    gBrowser.tabContainer._getDropIndex = function(event, isLink) {
+        var tabs = this.allTabs;
+        var tab = this._getDragTargetTab(event, isLink);
+        if (!RTL_UI) {
+            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++) {
+                if (
+                    event.screenY <
+                    tabs[i].screenY + tabs[i].getBoundingClientRect().height
+                ) {
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i;
+                    }
+                    if (
+                        event.screenX >
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2 &&
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width
+                    ) {
+                        return i + 1;
+                    }
+                }
+            }
+        } else {
+            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++) {
+                if (
+                    event.screenY <
+                    tabs[i].screenY + tabs[i].getBoundingClientRect().height
+                ) {
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width &&
+                        event.screenX >
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i;
+                    }
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i + 1;
+                    }
+                }
+            }
+        }
+        return tabs.length;
+    }
 }
--- Mutirowtabs/Firefox-69/6-MultiRowTabLiteforFx.uc.js
+++ Mutirowtabs/Firefox-69/6-MultiRowTabLiteforFx.uc.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name           zzzz-MultiRowTab_LiteforFx48.uc.js
 // @namespace      http://space.geocities.yahoo.co.jp/gl/alice0775
-// @description    多段タブもどき実験版 CSS入れ替えまくりLiteバージョン
+// @description    Experimentelle CSS Version für Mehrzeilige Tableiste
 // @include        main
-// @compatibility  Firefox 67
+// @compatibility  Firefox 69
 // @author         Alice0775
 // @version        2016/08/05 00:00 Firefox 48
 // @version        2016/05/01 00:01 hide favicon if busy
@@ -14,36 +14,40 @@
 "user strict";
 MultiRowTabLiteforFx();
 function MultiRowTabLiteforFx() {
-    var css =`
-    /* Tableiste unter Adressleiste verschieben */
-    #titlebar { -moz-box-ordinal-group: 2; -moz-appearance: none !important; }
-    /* Menüleiste nach oben verschieben */
-    #toolbar-menubar { position: fixed; display: inline-flex; flex-wrap: nowrap; left:0; }
-    [sizemode="maximized"]:not([tabsintitlebar="true"]) #toolbar-menubar,
-    [sizemode="normal"] #toolbar-menubar { top: 1px; }
-    [sizemode="maximized"] #toolbar-menubar { top: 8px; }
-    /* Titelleisten-Schaltfläche nach oben verschieben */
-    #navigator-toolbox:not([style^="margin-top:"])[style=""] #window-controls,
-    [tabsintitlebar="true"] .titlebar-buttonbox-container { position: fixed; right:0; }
-    [tabsintitlebar="true"][sizemode="normal"] .titlebar-buttonbox-container { top: 1px; }
-    [tabsintitlebar="true"][sizemode="maximized"] .titlebar-buttonbox-container { top: 8px; }
-    #navigator-toolbox:not([style^="margin-top:"])[style=""] #window-controls { top: 0; }
-    /* Menüleiste über der Symbolleiste Platz zuweisen */
-    [sizemode="maximized"]:not([tabsintitlebar="true"]) #navigator-toolbox,
-    [sizemode="normal"] #navigator-toolbox { padding-top: 28px !important; }
-    [sizemode="maximized"] #navigator-toolbox { padding-top: 36px !important; }
-    /* Anderen leeren Bereich als die Menü- oder Titelleistenschaltflächen als Ziehbereich angeben */
-    [tabsintitlebar="true"] #navigator-toolbox { -moz-window-dragging:drag; }
-    /* Auf der rechten Seite Platz für die Schaltflächen der Titelleiste zuweißen, damit die   
-	Schaltflächen der Titelleiste und der Naigationssymbolleiste nicht verdeckt werden */
-    [sizemode="fullscreen"] #nav-bar { padding-right: 109px !important; }
+    /* Symbolleisten und Menüleiste von der Titelleiste in die Navigator-Toolbox verschieben */
+    document.getElementById("navigator-toolbox").appendChild(document.getElementById("toolbar-menubar"));
+    var css =` @-moz-document url-prefix("chrome://browser/content/browser.xhtml") {
+    /* Symbolleisten Sortieren */
+    #toolbar-menubar { -moz-box-ordinal-group: 1 !important; }
+    #nav-bar { -moz-box-ordinal-group: 2 !important; }
+    #PersonalToolbar { -moz-box-ordinal-group: 3 !important; }
+    #titlebar { -moz-box-ordinal-group: 4 !important; }
+    /* Symbolleisten Anpassungen */
+    #nav-bar { z-index: 2 !important; }
+    [tabsintitlebar="true"][sizemode="maximized"] #navigator-toolbox { padding-top: 8px !important; }
+    [tabsintitlebar="true"][sizemode="maximized"] #titlebar { -moz-appearance: none !important; }
+    [tabsintitlebar="true"] #toolbar-menubar { height: 32px; }
+    /* Titelleistenschaltfläche ausblenden und anzeigen, wenn Sie den Mauszeiger über die rechte obere Ecke bewegen */
+    /* Bereich beim Überfahren mit der Maus reagieren lassen und Titelleistenschaltflächen oben rechts verbergen,
+       um Bereich transparent zu machen */
+    #navigator-toolbox:not([style^="margin-top:"])[style=""] #TabsToolbar>#window-controls,
+    [tabsintitlebar="true"] #TabsToolbar>.titlebar-buttonbox-container { opacity: 0; right: -80px;
+        position: fixed; z-index: 2147483647 !important; background-color: hsla(0, 0%, 60%, 0.5); }
+    [tabsintitlebar="true"][sizemode="normal"] #TabsToolbar>.titlebar-buttonbox-container { top: -21px; }
+    [tabsintitlebar="true"][sizemode="maximized"] #TabsToolbar>.titlebar-buttonbox-container { top: -14px; }
+    #navigator-toolbox:not([style^="margin-top:"])[style=""] #TabsToolbar>#window-controls { top: -18px; }
+    /* npassung für Titelleistenschaltflächen */
+    #navigator-toolbox:not([style^="margin-top:"])[style=""] #TabsToolbar>#window-controls:hover,
+    [tabsintitlebar="true"] #TabsToolbar>.titlebar-buttonbox-container:hover { opacity: 1; right:0; }
+    [tabsintitlebar="true"][sizemode="normal"] #TabsToolbar>.titlebar-buttonbox-container:hover { top: 1px; }
+    [tabsintitlebar="true"][sizemode="maximized"] #TabsToolbar>.titlebar-buttonbox-container:hover { top: 8px; }
+    #navigator-toolbox:not([style^="margin-top:"])[style=""] #TabsToolbar>#window-controls:hover { top: 0; }
     /* Mehrzeilige Tableiste */
     tabs>arrowscrollbox{display:block;}
     tabs arrowscrollbox>scrollbox {
-        display:flex;flex-wrap:wrap;
+        display:flex;display:-webkit-box;flex-wrap:wrap;
         max-height: calc(var(--tab-min-height) * 5); /* Anzahl der Tabzeilen */
-        overflow-x:hidden;overflow-y:auto;
-    }
+        overflow-x:hidden;overflow-y:auto; }
     [tabsintitlebar="true"] tabs scrollbar{-moz-window-dragging:no-drag;} 
 	/* Bei Überschreitung der angegebenen Zeilenanzahl, mit der Maus,    
 	   über die dann eingeblendetet Scrolleiste zu Zeile wechseln */
@@ -51,20 +55,17 @@ function MultiRowTabLiteforFx() {
     tabs tab,.tab-background {
         height: var(--tab-min-height);
         overflow: hidden;
-        z-index: 1 !important;
-    }
+        z-index: 1 !important; }
     tab>.tab-stack{width:100%;}
-    [sizemode="fullscreen"] #TabsToolbar>#window-controls,
-    .titlebar-buttonbox-container>.titlebar-buttonbox{display:block;}
     /* Drag-Bereich auf der linken und rechten Seite der
        Tab-Leiste auslenden - verstecken
        Links und rechts → hbox.titlebar-spacer 
        links → hbox.titlebar-spacer[type="pre-tabs"] 
        rechts → hbox.titlebar-spacer[type="post-tabs"] */
-    hbox.titlebar-spacer,
+    hbox.titlebar-spacer
     /* Ausblenden - Verstecken */
-    #alltabs-button,tabs [class^="scrollbutton"],tabs spacer,[autohide="true"][inactive="true"] .titlebar-buttonbox { display: none; }
-    `;
+    ,#alltabs-button,tabs [class^="scrollbutton"],tabs spacer,#toolbar-menubar .titlebar-buttonbox { display: none; }
+    } `;
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
     sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
@@ -79,23 +80,23 @@ function MultiRowTabLiteforFx() {
     'type="text/css" href="data:text/css,' + encodeURIComponent(style) + '"');
     document.insertBefore(sspi, document.documentElement);
     gBrowser.tabContainer._animateTabMove = function(event){}
-    gBrowser.tabContainer._finishAnimateTabMove = function(event){}
+    gBrowser.tabContainer._finishAnimateTabMove = function(){}
     gBrowser.tabContainer.lastVisibleTab = function() {
-        var tabs = this.children;
+        var tabs = this.allTabs;
         for (let i = tabs.length - 1; i >= 0; i--){
             if (!tabs[i].hasAttribute("hidden"))
                 return i;
         }
         return -1;
-    };
+    }
     gBrowser.tabContainer.clearDropIndicator = function() {
-        var tabs = this.children;
+        var tabs = this.allTabs;
         for (let i = 0, len = tabs.length; i < len; i++){
             let tab_s= tabs[i].style;
             tab_s.removeProperty("border-left-color");
             tab_s.removeProperty("border-right-color");
         }
-    };
+    }
     gBrowser.tabContainer.addEventListener("dragleave",gBrowser.tabContainer.clearDropIndicator, false);
     gBrowser.tabContainer._onDragOver = function(event) {
         event.preventDefault();
@@ -104,48 +105,100 @@ function MultiRowTabLiteforFx() {
         var newIndex = this._getDropIndex(event);
         if (newIndex == null)
             return;
-        if (newIndex < this.children.length) {
-            this.children[newIndex].style.setProperty("border-left-color","red","important");
+        let children = this.allTabs;
+        if (newIndex < children.length) {
+            children[newIndex].style.setProperty("border-left-color","red","important");
         } else {
             newIndex = gBrowser.tabContainer.lastVisibleTab();
             if (newIndex >= 0)
-                this.children[newIndex].style.setProperty("border-right-color","red","important");
+                children[newIndex].style.setProperty("border-right-color","red","important");
         }
-    };
-    gBrowser.tabContainer.addEventListener("dragover", gBrowser.tabContainer._onDragOver, false);
-    gBrowser.tabContainer._getDropIndex = function(event, isLink) {
-        var tabs = this.children;
-        var tab = this._getDragTargetTab(event, isLink);
-        if (!RTL_UI) {
-            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++)
-                if (event.screenX < tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
-                 && event.screenY < tabs[i].screenY + tabs[i].getBoundingClientRect().height) // multirow fix
-                    return i;
-        } else {
-            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++)
-                if (event.screenX > tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
-                 && event.screenY > tabs[i].screenY + tabs[i].getBoundingClientRect().height) // multirow fix
-                    return i;
-        }
-        return tabs.length;
     }
+    gBrowser.tabContainer.addEventListener("dragover", gBrowser.tabContainer._onDragOver, false);
     gBrowser.tabContainer.onDrop = function(event) {
         this.clearDropIndicator();
         var dt = event.dataTransfer;
         var draggedTab;
         if (dt.mozTypesAt(0)[0] == TAB_DROP_TYPE) {
             draggedTab = dt.mozGetDataAt(TAB_DROP_TYPE, 0);
-            if (!draggedTab)
+            if (!draggedTab) {
                 return;
+            }
         }
-        this._tabDropIndicator.collapsed = true;
+        this._tabDropIndicator.hidden = true
         event.stopPropagation();
-        if (draggedTab && draggedTab.parentNode == this) {
+        if (draggedTab && draggedTab.container == this) {
             let newIndex = this._getDropIndex(event, false);
             if (newIndex > draggedTab._tPos)
                 newIndex--;
             gBrowser.moveTabTo(draggedTab, newIndex);
         }
-    };
+    }
     gBrowser.tabContainer.addEventListener("drop",gBrowser.tabContainer.onDrop, false);
+    gBrowser.tabContainer._getDragTargetTab = function(event, isLink) {
+        let tab = event.target;
+        while (tab && tab.localName != "tab") {
+            tab = tab.parentNode;
+        }
+        if (tab && isLink) {
+            let { width } = tab.getBoundingClientRect();
+            if (
+                event.screenX < tab.screenX + width * 0.25 ||
+                event.screenX > tab.screenX + width * 0.75
+            ) {
+                return null;
+            }
+        }
+        return tab;
+    }
+    gBrowser.tabContainer._getDropIndex = function(event, isLink) {
+        var tabs = this.allTabs;
+        var tab = this._getDragTargetTab(event, isLink);
+        if (!RTL_UI) {
+            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++) {
+                if (
+                    event.screenY <
+                    tabs[i].screenY + tabs[i].getBoundingClientRect().height
+                ) {
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i;
+                    }
+                    if (
+                        event.screenX >
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2 &&
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width
+                    ) {
+                        return i + 1;
+                    }
+                }
+            }
+        } else {
+            for (let i = tab ? tab._tPos : 0; i < tabs.length; i++) {
+                if (
+                    event.screenY <
+                    tabs[i].screenY + tabs[i].getBoundingClientRect().height
+                ) {
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width &&
+                        event.screenX >
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i;
+                    }
+                    if (
+                        event.screenX <
+                        tabs[i].screenX + tabs[i].getBoundingClientRect().width / 2
+                    ) {
+                        return i + 1;
+                    }
+                }
+            }
+        }
+        return tabs.length;
+    }
 }
