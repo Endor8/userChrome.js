aab1d00cdb3a8bcc6f9b8ed23c91f02e249386b6
Endor8 <endor8@gmail.com>
2021-12-26 14:41:02 +0100

    Update 03-MultiRowTabLiteforFx.uc.js

--- Mutirowtabs/Firefox-95+/03-MultiRowTabLiteforFx.uc.js
+++ Mutirowtabs/Firefox-95+/03-MultiRowTabLiteforFx.uc.js
@@ -50,7 +50,8 @@ function MultiRowTabLiteforFx() {
        können Sie den Ziehbereich links einblenden, der beim Maximieren des Fensters ausgeblendet wird. */
     /* :root:not([sizemode="normal"]) hbox.titlebar-spacer[type="pre-tabs"] { display: block !important; } */
 
-    /* ↓ Wenn Sie die Auskommentierung links und rechts von unten stehenden CSS-Code entfernen und den CSS-Code aktivieren, können Sie den linken und rechten Ziehbereich einblenden, der im Vollbildmodus ausgeblendet wird.   */
+    /* ↓ Wenn Sie die Auskommentierung links und rechts von unten stehenden CSS-Code entfernen und den CSS-Code aktivieren,    
+         können Sie den linken und rechten Ziehbereich einblenden, der im Vollbildmodus ausgeblendet wird.   */
     /* :root[inFullscreen] .titlebar-spacer { display: block !important; } */
 
     } `;
