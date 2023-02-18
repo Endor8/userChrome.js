341e1dc7cf6a06ff066d2e7816bf1cf24e05af67
Endor8 <endor8@gmail.com>
2023-02-18 11:18:29 +0100

    Aktualisierung vom Autor

--- newtabbylongpress/NewTabByLongPress.uc.js
+++ newtabbylongpress/NewTabByLongPress.uc.js
@@ -1,81 +1,108 @@
 // ==UserScript==
 // @name                newTabByLongPress.uc.js
-// @description         リンクやブックマークを左ボタン長押しで新しいタブに開く
+// @description         Lesezeichen, auch aus Seitenleiste, mit Links-klick und länger gedrückt gehaltener Maustaste in neuem Tab öffnen.
 // @include             main
-// @version             0.8  コードを書き直し 不要になったリスナーを破棄するように改善
+// @version             0.8.7  Fx110+
+// @version             0.8.6  Kleine Korrektur
+// @version             0.8.5  Funktionierte nicht mehr in der Seitenleiste, behoben.
+// @version             0.8.4  Spezifikationen von Lesezeichen geändert
 // ==/UserScript==
-(function func(win) {
+(function () {
+	'use strict';
 
-	const WAIT = 500; // Zeit für die Dauer des Tastendruck
-	const IN_BACKGROUND = true; // "false" für das Öffnen im Vordergrund
-	const RELATED_TO_CURRENT = true; // Direkt neben dem aktuellen Tab öffnen
+	const WAIT = 280; // Zeit für die Dauer des Tastendruck festlegen
+	const IN_BACKGROUND = true; // "false" = Tabs im Vordergrund öffnen
+	const RELATED_TO_CURRENT = true; // Lesezeichen direkt neben dem aktuellen Tab öffnen
 
-	var longPress = null;
+	var timeoutID;
+	var longPress = false;
 
-	win.addEventListener('mousedown', {
-		handleEvent: function (e) {
-			if (e.button !== 0 && longPress) return;
+	function handleLongPress (event) {
+		if (timeoutID) {
+			clearTimeout(timeoutID);
+			timeoutID = null;
+		}
 
-			clearTimeout(func.timer);
+		if (event.button !== 0) return;
+		if (event.altKey || event.ctrlKey || event.shiftKey) return;
 
-			var target = e.target || e.originalTarget;
-			if (!target) return;
+		var node = event.target || event.originalTarget;
+		if (!checkNode(node)) return;
+		
+		var url = getPlacesURI(event, node);
+		if (!url) return;
 
-			var [href,] = hrefAndLinkNodeForClickEvent(e);
-			var url = href || this.getPlacesURI(e, target);
-			if (!url) return;
-
-			if (e.type === 'mousedown' && (!e.altKey && !e.ctrlKey && !e.shiftKey)) {
-				func.timer = setTimeout(function () {
-					win.addEventListener('click', function c(e) {
-						this.removeEventListener(e.type, c, true);
-						e.preventDefault();
-						e.stopPropagation();
-					}, true);
-					gBrowser.loadOneTab(url, {
-						relatedToCurrent: RELATED_TO_CURRENT,
-						inBackground: IN_BACKGROUND,
-						referrerURI: makeURI(gBrowser.currentURI.spec)
-					});
-					longPress = true;
-				}, WAIT);
-				win.addEventListener('mouseup', this, true);
-				win.addEventListener('dragstart', this, true);
-			} else {
-				win.removeEventListener('mouseup', this, true);
-				win.removeEventListener('dragstart', this, true);
-				clearTimeout(func.timer);
-				if (longPress && (target._placesNode && PlacesUtils.nodeIsURI(target._placesNode))) {
-					e.preventDefault();
-				}
+		if (event.type === 'mousedown') {
+			timeoutID = setTimeout(function () {
+				addEventListener('click', function clk(event) {
+					removeEventListener('click', clk, true);
+					event.preventDefault();
+					event.stopPropagation();
+				}, true);
+				gBrowser.addTab(url, {
+					relatedToCurrent: RELATED_TO_CURRENT,
+					inBackground: IN_BACKGROUND,
+					referrerURI: makeURI(gBrowser.currentURI.spec),
+					triggeringPrincipal: Services.scriptSecurityManager.createNullPrincipal({})
+				});
+				longPress = true;
+			}, WAIT);
+		} else {
+			clearTimeout(timeoutID);
+			if (longPress && event.type === 'mouseup') {
+				event.preventDefault();
 				longPress = false;
 			}
-		},
-
-		getPlacesURI: function (e, target) {
-			if (!target || !target.localName)
-				return null;
-			var ln = target.localName.toLowerCase();
-			if ((target._placesNode && PlacesUtils.nodeIsURI(target._placesNode)) || (ln === 'treechildren' && (target.parentNode.id === 'bookmarks-view' || target.parentNode.id === 'historyTree'))) {
-				var nodeURI = (ln === 'treechildren') ? this.getTreeInfo(target, e, 'uri') : target._placesNode.uri;
-			}
-			return nodeURI;
-		},
-
-		getTreeInfo: function (treechildren, e, prop) {
-			if (!('PlacesUtils' in window))
-				return '';
-			var tree = treechildren.parentNode;
-			var row = {}, col = {}, obj = {};
-			var tbo = tree.treeBoxObject;
-			tbo.getCellAt(e.clientX, e.clientY, row, col, obj);
-			if (row.value === -1)
-				return '';
-			var node = tree.view.nodeForTreeIndex(row.value);
-			if (!PlacesUtils.nodeIsURI(node))
-				return '';
-			return node[prop];
 		}
-	}, true);
+	}
 
-}(window));
+	function checkNode (node) {
+		if (!node || !node.localName) {
+			return;
+		}
+		var ln = node.localName.toLowerCase();
+		return (node._placesNode && PlacesUtils.nodeIsURI(node._placesNode)
+			|| (ln === 'treechildren' && (isBookmarkTree(node.parentNode)
+			|| isHistoryTree(node.parentNode))))
+	}
+	
+	function getPlacesURI (event, node) {
+		var ln = node.localName.toLowerCase();
+		return (ln === 'treechildren') ? getTreeInfo(event, node).uri : node._placesNode.uri;
+	}
+
+	function getTreeInfo (event, node) {
+		if (!('PlacesUtils' in window)) {
+			return '';
+		}
+		var treechildren = node;
+		if (treechildren.localName !== 'treechildren') {
+			return;
+		}
+		var tree = treechildren.parentNode;
+		var cell = tree.getCellAt(event.clientX, event.clientY);
+		if (cell.row === -1) {
+			return '';
+		}
+		var url = tree.view.nodeForTreeIndex(cell.row);
+		if (!url || !PlacesUtils.nodeIsURI(url)) {
+			return '';
+		}
+		return url;
+	}
+
+	function isBookmarkTree(tree) {
+		return /[:&]folder=/.test(tree.getAttribute('place'));
+	}
+
+	function isHistoryTree(tree) {
+		var place = tree.getAttribute('place');
+		return !/[:&]folder=/.test(place)
+			&& !/[:&]transition=7(?:&|$)/.test(place);
+	}
+
+	['mousedown', 'mouseup', 'dragstart'].forEach(function (type) {
+		addEventListener(type, handleLongPress, true);
+	});
+
+}());
