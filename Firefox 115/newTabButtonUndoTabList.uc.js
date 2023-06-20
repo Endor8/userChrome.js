6a2a88f40306a321bb771d32f6706cc595c3f62b
Endor8 <endor8@gmail.com>
2023-06-20 13:31:47 +0200

    Update newTabButtonUndoTabList.uc.js

--- Firefox 115/newTabButtonUndoTabList.uc.js
+++ Firefox 115/newTabButtonUndoTabList.uc.js
@@ -3,8 +3,7 @@
 // @description    Bei Rechtsklick auf die Neuen Tab Schaltfläche, wird im Kontextmenü,
 // @description    der Eintrag zum Wiederherstellen des zuletzt geschlossenen Tabs angezeigt.
 // @Versionsinfo   Aktualisierung für Firefox 115+. Nicht kompatibel mit Firefox 114 und älter!
-// @Info           Ab Firefox 115 wegen Bug 1819675 ⇒ https://bugzilla.mozilla.org/show_bug.cgi?id=1819675
-// @Info           Änderung notweng.
+// @Info           Anpassung Firefox 115-Bug 1819675 ⇒ https://bugzilla.mozilla.org/show_bug.cgi?id=1819675
 // @include        main
 // ==/UserScript==
 (function () {
