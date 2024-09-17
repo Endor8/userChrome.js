b32c5dcec0d64689d1a8c70aa1557346bdf06b95
Endor8 <endor8@gmail.com>
2024-09-17 07:59:16 +0200

    Add files via upload

---
+++ Firefox 128/closetab.uc.js
@@ -0,0 +1,26 @@
+// buttonCloseTab.uc.js - Button > Firefox Tabs schliessen
+(function() {
+	if (location != 'chrome://browser/content/browser.xhtml')
+      return;
+
+try {
+	ChromeUtils.importESModule("resource:///modules/CustomizableUI.sys.mjs");
+	CustomizableUI.createWidget({
+		id: "fp-closetabss",
+		defaultArea: CustomizableUI.AREA_NAVBAR,
+		removable: true,
+		label: "Aktuellen Tab schließen",
+		tooltiptext: "Aktuellen Tab schließen",
+        onClick: function(event) {
+            event.target.ownerGlobal.BrowserCommands.closeTabOrWindow();
+		},
+		onCreated: function(aNode) {
+			aNode.style.listStyleImage = 'url(\'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 22 22"><path fill="currentColor" fill-opacity="context-fill-opacity" d="M9.414 8l5.293-5.293a1 1 0 0 0-1.414-1.414L8 6.586 2.707 1.293a1 1 0 0 0-1.414 1.414L6.586 8l-5.293 5.293a1 1 0 1 0 1.414 1.414L8 9.414l5.293 5.293a1 1 0 0 0 1.414-1.414z"></path></svg>\')';
+			return aNode;
+		}
+	});
+    CustomizableUI.registerToolbarNode(tb);
+} catch (e) {
+	Components.utils.reportError(e);
+};
+})();
