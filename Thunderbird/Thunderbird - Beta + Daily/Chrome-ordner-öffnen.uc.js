a9aa35668a0dc4ef2a6371c9749915ed504b1e32
Endor8 <endor8@gmail.com>
2025-06-29 16:58:15 +0200

    Update Chrome-ordner-öffnen.uc.js

--- Thunderbird/Thunderbird - Beta + Daily/Chrome-ordner-öffnen.uc.js
+++ Thunderbird/Thunderbird - Beta + Daily/Chrome-ordner-öffnen.uc.js
@@ -10,18 +10,17 @@
     var props = {
             id: 'chromefolder-ToolBarButton',
             class: 'toolbarbutton-1',
-            label: 'Chrome-Ordner',
+            label: '',
             tooltiptext: 'Chromeordner öffnen',
-            style: 'list-style-image: url("file:///F:/Adaten/Downloads/Firefox/Thunderbird/Thunderbird Setup 102.0.3/thunderbird-114.0a1.de.win64/Profilordner/chrome/icons/Chrome-Ordner.png")',
-            onclick: 'if (event.button == 0) { \
-Services.dirsvc.get("UChrm", Ci.nsIFile).launch(); \
-}; '
+            style: 'list-style-image: url("..//icons/Chrome-Ordner.png")',
                 };
-                for (var p in props) toolbarbutton.setAttribute(p, props[p]);
+                for (var p in props) 
+				toolbarbutton.setAttribute(p, props[p]);
+			    toolbarbutton.addEventListener('click', event => {
+				if (event.button == 0) {Services.dirsvc.get("UChrm", Ci.nsIFile).launch()
+                 }
+				});				   
 
-    var toolbox = document.getElementById("mail-toolbox");
-    toolbox.palette.appendChild(toolbarbutton);    
-
-    var toolbar = document.getElementById("tabbar-toolbar");
-    toolbar.insertItem("chromefolder-ToolBarButton", toolbar.lastChild);
+    var position = document.getElementById('unifiedToolbarContent');    
+    position.parentNode.insertBefore(toolbarbutton, position.nextSibling);
         })();
